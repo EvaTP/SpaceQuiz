1401d90d1134ce4541bb8a27c79da89bae2dae35
EvaTP <evathpr@icloud.com>
2025-03-30 18:21:56 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# PROJET SPACEQUIZ
+
+Ce projet a été réalisé en mars 2025 dans le cadre d'un exercice collectif au sein de l'école ADA TECH SCHOOL.
+
+## Description
+
+L'objectif de ce projet était de développer une application de quiz interactive en utilisant des technologies front-end telles que HTML, CSS et JavaScript.
+
+Le but principal était de permettre aux utilisateur·ices de répondre à une série de questions et d'évaluer leurs connaissances en temps réel, avec un retour immédiat sur leurs réponses ou à la fin du quiz.
+
+Un autre objectif était d'apprendre à travailler en équipe en utilisant les méthodes agiles et un outil de gestion de proje.
+
+
+## Stack
+
+- JavaScript
+- HTML
+- CSS
+- Autres : Canva, outil Trello
+
+
+## Equipe
+
+Eva THARRATS
+Romain CHOUHANI
+Hanane BENABID
+
+## Crédits
+<a href="http://www.freepik.com">Designed by brgfx / Freepik</a>
+
+
+
+
---
+++ questions.js
@@ -0,0 +1,105 @@
+export const quiz_espace = {
+    intro:  "Voulez-vous revoir votre culture générale sur l'espace ? Bonne nouvelle ! Avec ce quiz vous allez mettre en orbite vos connaissances sur l'espace !! 🚀" + `\n`+ "Top chrono, vous avez 10 secondes par question",
+      questions: [
+        {
+          text: "Combien de planètes y a t'il dans le système solaire ? ",
+          options: [
+            "1. Six",
+            "2. Neuf",
+            "3. Douze",
+            "4. Huit"
+          ],
+          correct_answer: "4. Huit"
+        },
+        {
+          text: "Quel est le satellite le plus célèbre de Jupiter ?",
+          options: [
+            "1. Europe",
+            "2. Afrique",
+            "3. Océanie",
+            "4. Asie"
+          ],
+          correct_answer: "1. Europe"
+        },
+        {
+          text: "Quelle est la durée moyenne d'une rotation complète de la Terre sur son axe ?",
+          options: [
+            "1. 365 jours",
+            "2. 24 heures",
+            "3. 1 heure",
+            "4. Le temps que le mari décide de vider le lave-vaisselle"
+          ],
+          correct_answer: "2. 24 heures"
+        },
+        {
+          text: "Dans quelle galaxie se situe le système solaire ?",
+          options: [
+            "1. Galaxie d'Andromède",
+            "2. Galaxie Samsung",
+            "3. Galaxie de la roue de Chariot",
+            "4. Galaxie La Voie Lactée"
+          ],
+          correct_answer: "4. Galaxie La Voie Lactée"
+        },
+        {
+          text: "Qu'est-ce qu'une constellation ?",
+          options: [
+            "1. Un groupe cosmique d'étoiles, planètes, de gaz et de poussière",
+            "2. Un groupe d'étoiles voisines",
+            "3. Un groupe de signes du Zodiaque",
+            "4. Un objet céleste composé de gaz raréfié, de plasma et de poussières interstellaires"
+          ],
+          correct_answer: "2. Un groupe d'étoiles voisines"
+        },
+        {
+          text: "Quelle était la mission d'Apollo 13 ?",
+          options: [
+            "1. Récupèrer des échantillons sur Mars",
+            "2. Rechercher des signes de vie sur Vénus",
+            "3. Faire arriver des hommes sur la Lune",
+            "4. Faire des sorties extra-véhiculaires sur la Lune"
+          ],
+          correct_answer: "3. Faire arriver des hommes sur la Lune"
+        },
+        {
+          text: "Qu'est ce qu'une nébuleuse ?",
+          options: [
+            "1. Un nuage d'étoiles",
+            "2. Un objet céleste composé de gaz raréfié, de plasma ou de poussières interstellaire",
+            "3. Une station spatiale",
+            "4. Un bug dans un programme informatique"
+          ],
+          correct_answer: "2. Un objet céleste composé de gaz raréfié, de plasma ou de poussières interstellaire"
+        },
+        {
+          text: "D'où décollent les fusées françaises ?",
+          options: [
+            "1. De l'aéroport du Bourget",
+            "2. De Cap Canaveral",
+            "3. De Kourou en Guyane",
+            "4. De centre spatial français en Martinique"
+          ],
+          correct_answer: "3. De Kourou en Guyane"
+        },
+        {
+          text: "Qu'est ce que Spoutnik ?",
+          options: [
+            "1. La première fusée russe habitée envoyée dans l'espace",
+            "2. La capsule spatiale qui envoya le premier chat dans l'espace",
+            "3. Un gâteau russe à base de vodka et de crème au citron",
+            "4. Un programme de satellites russes"
+          ],
+          correct_answer: "4. Un programme de satellites russes"
+        },
+        {
+          text: "Quelle est la planète du système solaire la plus éloignée de la Terre ?",
+          options: [
+            "1. Neptune",
+            "2. Mars",
+            "3. Jupiter",
+            "4. Pluton"
+          ],
+          correct_answer: "1. Neptune"
+        }
+      ]
+    };
